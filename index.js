153acb1b9ca9d12b1e05fbf7d8f977b11e3bc5ff
bharper999 <115158668+bharper999@users.noreply.github.com>
2022-12-31 19:17:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const name ='name1';
+const height ='height1';
+const country ='country1';
+
+function stats(){
+console.log("Hello" + " " + name1);
+console.log("Your" + " " + height1);
+console.log("From" + " " + country1);
+}
+let name1=prompt('Enter a name');
+let height1=prompt('Enter height');
+let country1=prompt('Enter country')
+
+stats(name, height, country);
+
+
+
+
