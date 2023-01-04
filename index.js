296db99a6ca1263104a72b178f339c31e4fa75ab
bharper999 <115158668+bharper999@users.noreply.github.com>
2023-01-04 21:10:32 +0300

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ console.log("Hello" + " " + name1);
 console.log("Your" + " " + height1);
 console.log("From" + " " + country1);
 }
+
 let name1=prompt('Enter a name');
 let height1=prompt('Enter height');
 let country1=prompt('Enter country')
